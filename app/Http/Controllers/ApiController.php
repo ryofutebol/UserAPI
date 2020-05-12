a13d6566023c5b037d95ae2781866cbf768cb4d2
奥野龍 <okunoryo@MBAR.local>
2020-05-13 02:09:39 +0900

    first webAPI実装

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class ApiController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = User::create($request->all());
+        return response()->json([
+            'user_id' => $user->id
+        ], 201, [], JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+        if ($user) {
+            return response()->json([
+                'name' => $user->name,
+                'age' => $user->age
+            ], 200, [], JSON_UNESCAPED_UNICODE);
+        } else {
+            return response()->json([
+                'message' => '対象のレコードが見つかりません',
+            ], 404, [], JSON_UNESCAPED_UNICODE);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $update = [
+            'name' => $request->name,
+            'age' => $request->age
+        ];
+        $user = User::where('id', $id)->update($update);
+        if ($user) {
+            return response()->json([
+                'name' => $user->name,
+                'age' => $user->age,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => '対象のレコードが見つかりません',
+            ], 404, [], JSON_UNESCAPED_UNICODE);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::where('id', $id)->delete();
+        if ($user) {
+            return response()->json([
+                'message' => '対象のレコードを削除しました。',
+            ], 200, [], JSON_UNESCAPED_UNICODE);
+        } else {
+            return response()->json([
+                'message' => '対象のレコードが見つかりません',
+            ], 404, [], JSON_UNESCAPED_UNICODE);
+        }
+    }
+}
